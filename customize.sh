6afec8767cb2e2119c8fecc313e84a82da80bdaf
ydhang <42969436+ydhang@users.noreply.github.com>
2022-02-09 19:07:55 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -15,10 +15,10 @@ sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-se
 sed -i 's/R22.1.1/R22.1.1 Compiled by ydhang/g' openwrt/package/lean/default-settings/files/zzz-default-settings
 
 #3. Replace with Argon
-rm openwrt/package/lean/luci-theme-argon -rf
+#rm openwrt/package/lean/luci-theme-argon -rf
 
 #4. Install JerryKuKu’s new Argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git openwrt/package/lean/luci-theme-argon
+#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git openwrt/package/lean/luci-theme-argon
 
 #5. Change the default theme
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' openwrt/feeds/luci/collections/luci/Makefile
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' openwrt/feeds/luci/collections/luci/Makefile
